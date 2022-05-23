3f5845a910f626e2fa773a74589895dae7307eb3
Alex Kostin <askostin.mipt@yandex.ru>
2022-05-23 20:37:21 +0300

    1. Move gcd() from primes.py to dividers.py. 2. Add function encode_sternbrocot(), which works correctly for rational numbers but needs further development to work with float numbers. 3. Add auxiliary float_precision() and float2fraction() functions, which are to be developed.

--- dividers.py
+++ dividers.py
@@ -103,4 +103,125 @@ def evenize(n:int):
 	else:
 		return -1
 
+def float_precision(number: float) -> int:
+	"""
+	Find number of digits on the right sight of the decimal point.
+	"""
+	max_precision = 16
+	number_whole_part = math.trunc(number)
+	...
+
+def float2fraction(number: float) -> tuple:
+	"""
+	Converts decimal number to rational fraction in the form of tuple (n, d), where n and d are mutually prime.
+	"""
+	number_whole_part = math.trunc(number)
+	number_fraction_part = abs(number - number_whole_part)
+
+def SternBrocot_list(level: int, seq_type = 'Stern-Brocot', drop_inf = False):
+	"""
+	SternBrocot_list : N, str, bool -> listof((N, N))
+	SternBrocot_list(level, drop_inf) creates list of fractions, based on Stern-Brocot tree (in the interval [0, +inf]) or Farray sequence (in the interval [0, 1]):
+		- level (int) - depth of the tree, number of iterations when we insert new fractions;
+		- seq_type (str) - 'Stern-Brocot' (default) or 'Farray' sequence (list) to build;
+		- drop_inf (bool, default = False) - should we remove the tuple (1, 0)  (by definition, fraction 1/0 = +infinity) from Stern-Brocot output sequence.
+	Representation:
+		Fraction m/n is represented by tuple (m, n).
+	Examples:
+	1. For SternBrocot tree:
+		- list for level 0 tree: [(0, 1), (1, 0)]
+		- list for level 1 tree: [(0, 1), (1, 1), (1, 0)]
+		- list for level 2 tree: [(0, 1), (1, 2), (1, 1), (2, 1), (1, 0)]
+	2. For Farray sequence:
+		- list for level 0 tree: [(0, 1), (1, 1)]
+		- list for level 1 tree: [(0, 1), (1, 2), (1, 1)]
+		- list for level 2 tree: [(0, 1), (1, 3), (1, 1), (2, 3), (1, 1)]
+	Algorithm:
+		Between each two adjacent tuples (m, n) and (m', n') we place the new tuple (m + m', n + n').
+	"""
+
+	def insert_tuples(lst: list, n: int) :
+		if (n == 0) :
+			return lst
+		else :
+			iterations = len(lst) - 1
+			new_tuples = []
+			for i in range(iterations) :
+				nom = lst[i][0] + lst[i+1][0]
+				den = lst[i][1] + lst[i+1][1]
+				this_gcd = gcd(nom, den)
+				nom = int(nom / this_gcd)
+				den = int(den / this_gcd)
+				new_tuples.append( (nom, den) )
+			new_lst = []
+			for i in range(iterations) :
+				new_lst = new_lst + [lst[i], new_tuples[i]]
+			new_lst.append(lst[-1])
+			return insert_tuples(new_lst, n-1)
+
+	if level < 0 :
+		return ("Level should be >= 0")
+	else :
+		if (seq_type == 'Farray') :
+			lst = [(0, 1), (1, 1)]
+			result = insert_tuples(lst, level)
+		elif (seq_type == 'Stern-Brocot') :
+			lst = [(0, 1), (1, 0)]
+			result = insert_tuples(lst, level)
+			result = result[:-1] if drop_inf else result
+		return result
+
+def encode_sternbrocot(number, limit_length = True, max_length = 20) :
+	"""
+	encode_sternbrocot : number, limit_length, max_length -> stringof(S)
+	encode_sternbrocot : int/float/tuple, bool, int -> stringof(sym)
+	where:
+		- number can be:
+			* integer number,
+			* float number,
+			* tuple (m, n), which represents the rational fraction m/n;
+		- limit_length - whether we should constraint 
+		- S = {'L', 'R'}.
+	Write 'path' to a input fraction through the Stern-Brocot tree, where 'L' is for 'left', 'R' is for 'right'.
+	"""
+	def convert_pair(pair: tuple) :
+		m = pair[0]
+		n = pair[1]
+		if (n == 0) :
+			return 'RRR...'
+		this_gcd = gcd(m, n)
+		m = m / this_gcd
+		n = n / this_gcd
+		code = ''
+		while (m != n) :
+			if (m < n) :
+				code += 'L'
+				n = n - m
+			else :
+				code += 'R'
+				m = m - n
+		return code
+
+	if ((type(number) is tuple and len(number) == 2) :
+		return convert_pair(number)
+	elif (type(number) is int) :
+		return convert_pair((number, 1))
+	elif ((type(number) is float) and (number == math.floor(number))) :
+		return convert_pair((int(number), 1))
+	elif (type(number) is float) :
+		# 
+		code = ''
+		depth = 0
+		if (number < 1) :
+			code += 'L'
+			number = number / (1 - number)
+		elif (number > 1) :
+			code += 'R'
+			number = number / (number - 1)
+		else :
+			return code
+		# нужно дать ограничение на точность
+	else :
+		print("Input number should not be negative, and have to be integer, float, or rational fraction represented by tuple (m, n), where m and n are non-negative integers.")
+
 # Create function for maximal simplification of comparison by module based on eq. (4.37)-(4.40) from "Concrete Math"
