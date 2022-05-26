5c79639aae84bf8521162a00e4c2268bc5cded9f
Alex Kostin <askostin.mipt@yandex.ru>
2022-05-26 20:14:12 +0300

    Remove fact.py from package since its contains are in the __main__.py module

--- fact.py
+++
@@ -1,7 +0,0 @@
-import math
-
-def factorial_stirling(n: int) :
-    if (n < 0) :
-        raise ValueError('Error, input should be non-negative integer')
-    else :
-        return round(math.sqrt(2 * math.pi * n) * pow((n / math.e), n))
