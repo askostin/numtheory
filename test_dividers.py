466e474f5f72655fa4c8892c43d8014691c26933
Alex Kostin <askostin.mipt@yandex.ru>
2022-07-05 17:17:07 +0300

    test(dividers.py): add test file to test functions from dividers.py module

---
+++ test_dividers.py
@@ -0,0 +1,15 @@
+# Test our functions
+
+from .primes import *
+from .dividers import *
+
+def help():
+	print("test_gcd()")
+	print("test_simplify_frac()")
+	print("test_float2frac()")
+	print("test_divider_min()")
+	print("test_nondivisibles()")
+	print("test_first_divisible()")
+	print("test_sternbrocot_list()")
+	print("test_encode_sternbrocot()")
+	print("test_decode_sternbrocot()")
