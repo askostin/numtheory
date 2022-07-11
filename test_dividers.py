7878363c5745bec3608b2181bbd72a8de7b32ae1
askostin <askostin.mipt@yandex.ru>
2022-07-11 21:47:13 +0300

    test(dividers.py): add headers for tests for all functions in dividers.py

--- test_dividers.py
+++ test_dividers.py
@@ -13,3 +13,42 @@ def help():
 	print("test_sternbrocot_list()")
 	print("test_encode_sternbrocot()")
 	print("test_decode_sternbrocot()")
+
+
+def test_gcd():
+	input = [(0, 4), (4, 0), (6, 27), (15, 55), (33, 101), (100, 600)]
+	output_cor = [4, 4, 3, 5, 1, 100]
+	output = [gcd(tp) for tp in input]
+	for o, o_c in zip(output, output_cor):
+		...
+
+def test_simplify_frac():
+	pass
+
+
+def test_float2frac():
+	pass
+
+
+def test_divider_min():
+	pass
+
+
+def nondivisibles():
+	pass
+
+
+def first_divisible():
+	pass
+
+
+def sternbrocot_list():
+	pass
+
+
+def encode_sternbrocot():
+	pass
+
+
+def decode_sternbrocot():
+	pass
