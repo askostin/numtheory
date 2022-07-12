e1bf31e554aec7f59d52f239d0731dff1a5dc8aa
askostin <askostin.mipt@yandex.ru>
2022-07-12 22:39:19 +0300

    feat(test_dividers.py): add remaining tests

--- tests/test_dividers.py
+++ tests/test_dividers.py
@@ -1,6 +1,6 @@
 # Test our functions
 
-import numtheory.dividers as d
+from numtheory.dividers import *
 
 def help():
 	print("test_gcd()")
@@ -36,7 +36,7 @@ def general_test(fun, input, output_cor):
 
 def test_gcd():
 	general_test(
-		d.gcd,
+		gcd,
 		[[0, 4], [4, 0], [6, 27], [15, 55], [33, 101], [100, 600]],
 		[4, 4, 3, 5, 1, 100]
 	)
@@ -44,7 +44,7 @@ def test_gcd():
 
 def test_simplify_frac():
 	general_test(
-		d.simplify_frac,
+		simplify_frac,
 		[(0, 4), (6, 27), (15, 55), (-8, 32), (100, 600), (-12, 18)],
 		[(0, 1), (2, 9), (3, 11), (-1, 4), (1, 6), (-2, 3)]
 	)
@@ -52,7 +52,7 @@ def test_simplify_frac():
 
 def test_float2frac():
 	general_test(
-		d.float2frac,
+		float2frac,
 		[4, 1.25, 0, -8.3, 7.1, 2.04],
 		[(4, 1), (5, 4), (0, 1), (-83, 10), (71, 10), (51, 25)]
 	)
@@ -60,55 +60,88 @@ def test_float2frac():
 
 def test_divider_min():
 	general_test(
-		d.divider_min,
+		divider_min,
 		[2, 5, 7, 15, 37, 49, 51],
-		[2, 5, 7, 3, 37, 7, 17]
+		[2, 5, 7, 3, 37, 7, 3]
 	)
 
 
-def nondivisibles():
-	pass
+def test_nondivisibles():
+	general_test(
+		nondivisibles,
+		[[2, 10, [2, 3]], [12, 22, [3, 7]]],
+		[[5, 7], [13, 16, 17, 19, 20, 22]]
+	)
 
 
-def first_divisible():
-	pass
+def test_first_divisible():
+	general_test(
+		first_divisible,
+		[[2, 10, 3], [10, 20, 31], [15, 30, 15], [20, 40, 17]],
+		[3, None, 15, 34]
+	)
 
 
-def sternbrocot_list():
-	pass
+def test_sternbrocot_list():
+	general_test(
+		sternbrocot_list,
+		[[0],
+		 [1],
+		 [2, 'Stern-Brocot', True],
+		 [0, 'Farray'],
+		 [1, 'Farray'],
+		 [2, 'Farray', True]
+		],
+		[[(0, 1), (1, 0)],
+		 [(0, 1), (1, 1), (1, 0)],
+		 [(0, 1), (1, 2), (1, 1), (2, 1)],
+		 [(0, 1), (1, 1)],
+		 [(0, 1), (1, 2), (1, 1)],
+		 [(0, 1), (1, 3), (1, 2), (2, 3), (1, 1)]
+		]
+	)
 
 
-def encode_sternbrocot():
-	pass
+def test_encode_sternbrocot():
+	general_test(
+		encode_sternbrocot,
+		[[2.71828, True, 8], [(27, 10)], [(19, 7)]],
+		['RRLRRLRL', 'RRLRRLL', 'RRLRRL']
+	)
 
 
-def decode_sternbrocot():
-	pass
+def test_decode_sternbrocot():
+	general_test(
+		decode_sternbrocot,
+		['LRRL', 'RRLRRLL', 'RRLRRL'],
+		[(5, 7), (27, 10), (19, 7)]
+	)
+
 
 if __name__ == "__main__":
-	print("Testing gcd()")
+	print("\nTesting gcd():")
 	test_gcd()
 
-	print("Testing simplify_frac()")
+	print("\nTesting simplify_frac():")
 	test_simplify_frac()
 
-	print("Testing float2frac()")
+	print("\nTesting float2frac():")
 	test_float2frac()
 
-	print("Testing divider_min()")
+	print("\nTesting divider_min():")
 	test_divider_min()
 
-	print("Testing nondivisibles()")
+	print("\nTesting nondivisibles():")
 	test_nondivisibles()
 
-	print("Testing first_divisible()")
+	print("\nTesting first_divisible():")
 	test_first_divisible()
 
-	print("Testing sternbrocot_list()")
+	print("\nTesting sternbrocot_list():")
 	test_sternbrocot_list()
 
-	print("Testing encode_sternbrocot()")
+	print("\nTesting encode_sternbrocot():")
 	test_encode_sternbrocot()
 
-	print("Testing decode_sternbrocot()")
+	print("\nTesting decode_sternbrocot():")
 	test_decode_sternbrocot()
