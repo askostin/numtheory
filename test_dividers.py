3cb09f4a15457986f3d627892890846d9f0fb174
askostin <askostin.mipt@yandex.ru>
2022-07-12 14:26:37 +0300

    feat(test_dividers.py): add init code executing if module is launched as script

--- test_dividers.py
+++ test_dividers.py
@@ -19,8 +19,13 @@ def test_gcd():
 	input = [(0, 4), (4, 0), (6, 27), (15, 55), (33, 101), (100, 600)]
 	output_cor = [4, 4, 3, 5, 1, 100]
 	output = [gcd(tp) for tp in input]
-	for o, o_c in zip(output, output_cor):
-		...
+	flag = True
+	for i, o, o_c in zip(input, output, output_cor):
+		if (o != o_c):
+			flag = False
+			print("Fail: gcd({}) = {}, must be {}".format(i, o, o_c))
+	if flag:
+		print("Test passed")
 
 def test_simplify_frac():
 	pass
@@ -52,3 +57,31 @@ def encode_sternbrocot():
 
 def decode_sternbrocot():
 	pass
+
+if __name__ == "__main__":
+	print("Testing gcd()")
+	test_gcd()
+
+	print("Testing simplify_frac()")
+	test_simplify_frac()
+
+	print("Testing float2frac()")
+	test_float2frac()
+
+	print("Testing divider_min()")
+	test_divider_min()
+
+	print("Testing nondivisibles()")
+	test_nondivisibles()
+
+	print("Testing first_divisible()")
+	test_first_divisible()
+
+	print("Testing sternbrocot_list()")
+	test_sternbrocot_list()
+
+	print("Testing encode_sternbrocot()")
+	test_encode_sternbrocot()
+
+	print("Testing decode_sternbrocot()")
+	test_decode_sternbrocot()
